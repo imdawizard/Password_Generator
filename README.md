f429fd6fd8a0226f18a6999115749557af8a3f44
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-05-03 21:48:41 -0600

    Added border to btn, and created function for password generator

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password_Generator
+##Password_Generator
+
+#Description: This site is a modified version of a starter code given for instructional purposes.The starter code consisted of 3 files: html, css, and javascript. The html and css files were already completed. Then javascript was added in order to fulfill the purpose. That being, after clicking on the “Generate Password” button, a prompt will appear, asking the user for a length(between 8 and 128 characters), their preference on lower/upper case,. Special characters and numbers. After the user completes prompts a randomly generated password will appear on the screen within user specifications.
+
+#Additional Features:
+	-Some additional features include a catch, in case the user inputs a length that is less than 8 and greater than 128 characters. In addition, if the user does not activate any criteria, an alert will be activated, allowing the user to repick their criteria.
+	-A border was added to the hover ability on the “Generate Password” button, essentially giving it a ‘highlighted’ view.
+
+
+
+#Screenshot:
+
+#Link to deployed application:
+
+
--- develop/script.js
+++ develop/script.js
@@ -1,15 +1,70 @@
-// Assignment code here
+// Define character sets for lowercase, uppercase, numbers and special chars
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var nums = "0123456789";
+var specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+// #generate is the "generate password" btn
+
+function generatePassword() {
+  //Length of password
+  var passwordLength = prompt("Enter password length (between 8 and 128 characters):");
+  //Makes sure user input is between specified length
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Please enter a valid password length between 8 and 128 characters:")
+  }
+
+  //asks user for other criteria
+  var useLowerCase = confirm("Include Lowercase characters?");
+  var useUpperCase = confirm("Include Uppercase characters?");
+  var useNums = confirm("Include Numbers?");
+  var useSpecial = confirm("Include special characters?");
+
+  //Ensures at least one criteria was enabled
+  while (!(useLowerCase || useUpperCase || useNums || useSpecial)) {
+    alert("Please select at least one type of character type to include in your password.");
+    var useLowerCase = confirm("Include Lowercase characters?");
+    var useUpperCase = confirm("Include Uppercase characters?");
+    var useNums = confirm("Include Numbers?");
+    var useSpecial = confirm("Include special characters?");
+  }
+
+
+  //creates password
+  var password = "";
+  var charSet = "";
+
+  if (useLowerCase) {
+    charSet += lowercaseChars;
+  }
+  if (useUpperCase) {
+    charSet += uppercaseChars;
+  }
+  if (useNums) {
+    charSet += nums;
+  }
+  if (useSpecial) {
+    charSet += specialChars;
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+
+  return password;
+
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.textContent = password;
+  passwordText.setAttribute = ("syle", "font-size:100px", "font-weight:bold");
 
 }
 
